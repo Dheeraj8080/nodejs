9395d3e7e88be82af530280830c46b9dadabeeed
Dheeraj8080 <dheerajsharma121002@gmail.com>
2024-10-18 18:07:25 +0530

    TEST

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get("/", (req, res) => {
-  res.send("Home Page");
+app.get('/', (req, res) => {
+  res.send('Home Page');
 });
 
-app.get("/dashboard", (req, res) => {
-  res.send("DashBoard Page");
+app.get('/dashboard', (req, res) => {
+  res.send('DashBoard Page');
+});
+
+app.get('/test', (req, res) => {
+  res.send('test Page');
 });
 
 app.listen(port, () => {
-  console.log("Server Started, Port ", port);
+  console.log('Server Started, Port ', port);
 });
