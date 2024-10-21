dc44885dbdce2795ff8cf613dc381dabd407429a
Dheeraj8080 <dheerajsharma121002@gmail.com>
2024-10-21 13:02:51 +0530

    TEST3

--- server.js
+++ server.js
@@ -29,9 +29,6 @@ app.get('/test1', (req, res) => {
 app.get('/test1', (req, res) => {
   res.send('test Page');
 });
-app.get('/test1', (req, res) => {
-  res.send('test Page');
-});
 
 app.listen(port, () => {
   console.log('Server Started, Port ', port);
